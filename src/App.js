fe59fc37104bbd2196f3e8769c906ecb35bb44e4
GabeMoraes <gabriel.mfxc@outlook.com>
2023-12-12 21:34:48 -0300

    alpha App and new contract abi

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import Web3 from 'web3';
 
 
 function App() {
+  // setup metamask 
+  const web3 = new Web3(window.ethereum);
+  
   const [account, setAccount] = useState();
+  const [company, setCompany] = useState();
+
+  // create contract instance
+  const abi = require('./abis/Rent.json');
+  const contract = new web3.eth.Contract(abi,'0x5FbDB2315678afecb367f032d93F642f64180aa3');
   
   async function loadAccounts() {
-    const web3 = new Web3(window.ethereum);
     window.ethereum.enable();
     const accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
   }
 
-  useEffect(() => {
-    loadAccounts();
-  },[]);
+  async function loadCompany() {
+    const companyAddress = await contract.methods.getCompany().call();
+    setCompany(companyAddress);
+  }
 
   return (
     <div className="App">
       <header className='App-header'>
-        <p>
-          {account}
-        </p>
+        <p> Active Metamask account: {account} </p>
+        <p> Company: {company} </p>
+        <button onClick={loadAccounts}> Load accounts </button>
+        <button onClick={loadCompany}> Load company </button>
       </header>
     </div>
   );
